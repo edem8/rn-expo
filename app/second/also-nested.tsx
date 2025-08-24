a40f955cc7051f6d759b927ec66c4595a1edaaec
edem8 <eddieblack159@gmail.com>
2025-08-24 11:31:47 +0000

    feat:tab + nested stack

---
+++ app/second/also-nested.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function AlsoNested() {
+  return (
+    <View>
+      <Text>Also Nested</Text>
+    </View>
+  );
+}
---
+++ app/second/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+
+export default function IndexScreen() {
+  return (
+    <View className="flex-1 items-center p-4 justify-center">
+      <Text className="mb-4">second index</Text>
+
+      {/* Replace */}
+      <Link href="/second" push asChild>
+        <Pressable className="bg-red-500 w-[80vw] mt-2 p-3 rounded">
+          <Text className="text-white font-semibold text-center">
+            Path to nested
+          </Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
+}
+
+/**
+ * /index
+ * /second (stack)
+ *  /second/index
+ *  /second/nested
+ *  /second/also-nested
+ *
+ * /third
+ * /fourth
+ */
