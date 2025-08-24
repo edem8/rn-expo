12f6cfad2f9acdafaba016d0e75f4f93b0dd894e
edem8 <eddieblack159@gmail.com>
2025-08-24 16:14:38 +0000

    feat: Custom Modal

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,10 +1,11 @@
 import { Link, useRouter } from "expo-router";
-import React from "react";
-import { Alert, Pressable, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Modal, Pressable, Text, View } from "react-native";
 
 export default function index() {
   const router = useRouter();
   const canGoBack = router.canGoBack();
+  const [ModalVisible, setModalVisible] = useState(false);
   const handleOpenAlert = () => {
     Alert.alert("warning!", "Are you sure you want to proceed?", [
       { text: "Cancel", style: "cancel" },
@@ -46,10 +47,46 @@ export default function index() {
       {/* Alert Button */}
       <Pressable
         onPress={handleOpenAlert}
-        className="bg-pink-500 w-[80vw] mt-2 p-3 rounded"
+        className="bg-blue-500 w-[80vw] mt-2 p-3 rounded"
       >
         <Text className="text-white font-semibold text-center">Open Alert</Text>
       </Pressable>
+
+      {/* Modal Open Button */}
+      <Pressable
+        onPress={() => {
+          setModalVisible(true);
+        }}
+        className="bg-yellow-500 w-[80vw] mt-2 p-3 rounded"
+      >
+        <Text className="text-white font-semibold text-center">Open Modal</Text>
+      </Pressable>
+
+      {/* Modal */}
+      <Modal
+        visible={ModalVisible}
+        animationType="slide"
+        presentationStyle="pageSheet"
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View className="flex-1 items-center justify-center">
+          <View className="rounded-lg bg-white p-12">
+            <Text>A custom Styled Modal</Text>
+            <Pressable
+              onPress={() => {
+                setModalVisible(false);
+              }}
+              className="border border-gray-400 w-50 mt-2 p-3 rounded"
+            >
+              <Text className="text-gray-500 font-semibold text-center">
+                Close Modal
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
