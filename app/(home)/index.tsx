df6939e6e1e8b5237215076ee47860680805b44d
edem8 <eddieblack159@gmail.com>
2025-08-24 13:28:48 +0000

    feat: alert modal

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,10 +1,22 @@
 import { Link, useRouter } from "expo-router";
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Alert, Pressable, Text, View } from "react-native";
 
 export default function index() {
   const router = useRouter();
   const canGoBack = router.canGoBack();
+  const handleOpenAlert = () => {
+    Alert.alert("warning!", "Are you sure you want to proceed?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Confirm",
+        style: "destructive",
+        onPress: () => {
+          console.log("let's go!");
+        },
+      },
+    ]);
+  };
 
   return (
     <View className="flex-1 items-center p-4 justify-center">
@@ -30,6 +42,14 @@ export default function index() {
           <Text className="text-white font-semibold text-center">Back</Text>
         </Pressable>
       )}
+
+      {/* Alert Button */}
+      <Pressable
+        onPress={handleOpenAlert}
+        className="bg-pink-500 w-[80vw] mt-2 p-3 rounded"
+      >
+        <Text className="text-white font-semibold text-center">Open Alert</Text>
+      </Pressable>
     </View>
   );
 }
