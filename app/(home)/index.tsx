c76d442166d65a68e099fda9b37b62e5d8ea512b
edem8 <eddieblack159@gmail.com>
2025-08-24 13:03:28 +0000

    feat: Tab back behaviour

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,8 +1,11 @@
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import React from "react";
 import { Pressable, Text, View } from "react-native";
 
 export default function index() {
+  const router = useRouter();
+  const canGoBack = router.canGoBack();
+
   return (
     <View className="flex-1 items-center p-4 justify-center">
       <Text>Home index</Text>
@@ -15,6 +18,18 @@ export default function index() {
           </Text>
         </Pressable>
       </Link>
+
+      {/* Back Button */}
+      {canGoBack && (
+        <Pressable
+          onPress={() => {
+            router.back();
+          }}
+          className="bg-pink-500 w-[80vw] mt-2 p-3 rounded"
+        >
+          <Text className="text-white font-semibold text-center">Back</Text>
+        </Pressable>
+      )}
     </View>
   );
 }
--- app/second/index.tsx
+++ app/second/index.tsx
@@ -1,8 +1,10 @@
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import React from "react";
 import { Pressable, Text, View } from "react-native";
 
 export default function IndexScreen() {
+  const router = useRouter();
+  const canGoBack = router.canGoBack();
   return (
     <View className="flex-1 items-center p-4 justify-center">
       <Text className="mb-4">second index</Text>
@@ -15,6 +17,18 @@ export default function IndexScreen() {
           </Text>
         </Pressable>
       </Link>
+
+      {/* Back Button */}
+      {canGoBack && (
+        <Pressable
+          onPress={() => {
+            router.back();
+          }}
+          className="bg-pink-500 w-[80vw] mt-2 p-3 rounded"
+        >
+          <Text className="text-white font-semibold text-center">Back</Text>
+        </Pressable>
+      )}
     </View>
   );
 }
